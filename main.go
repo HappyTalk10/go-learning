b40bff50615b988d5659f32ef318af5833d802cb
HappyTalk10 <ueshima16@gmail.com>
2025-10-19 02:15:16 +0000

    Add go project files

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
-// main.go
 package main
 
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+    // 変数宣言
+    var name string = "田中太郎"
+    age := 25
+    height := 175.5
+    isStudent := true
+    
+    // 出力
+    fmt.Println("=== プロフィール ===")
+    fmt.Println("名前:", name)
+    fmt.Println("年齢:", age)
+    fmt.Println("身長:", height, "cm")
+    fmt.Println("学生:", isStudent)
 }
